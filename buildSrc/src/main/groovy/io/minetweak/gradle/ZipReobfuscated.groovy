360c3554cf33079177f59d5a3b671e71dccaf811
Logan Gorence <loganjohngorence@gmail.com>
2015-05-13 22:53:21 -0700

    Use Gradle Zip task.

--- buildSrc/src/main/groovy/io/minetweak/gradle/ZipReobfuscated.groovy
+++ buildSrc/src/main/groovy/io/minetweak/gradle/ZipReobfuscated.groovy
@@ -1,26 +1,15 @@
 package io.minetweak.gradle
 
-import net.lingala.zip4j.core.ZipFile
-import net.lingala.zip4j.model.ZipParameters
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.bundling.Zip
 
 /**
  * @author Logan Gorence
  */
-class ZipReobfuscated extends DefaultTask {
+class ZipReobfuscated extends Zip {
 
-    @TaskAction
-    void zipReobfuscated() {
-        new File("mtrelease.zip").delete()
-        ZipParameters zps = new ZipParameters()
-        zps.includeRootFolder = false
-        new ZipFile("mtrelease.zip").createZipFileFromFolder(
-                new File("mcp/reobf/minecraft_server/"),
-                zps,
-                false,
-                0L
-        )
+    public ZipReobfuscated() {
+        baseName = "Minetweak"
+        from "mcp/reobf/minecraft_server/"
     }
 
 }
